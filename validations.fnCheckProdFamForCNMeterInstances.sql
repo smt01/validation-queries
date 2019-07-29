ed0bcc715545e4076ae76140a82920f2cedb02be
Shubhang Tripathi <t-shtri@microsoft.com>
2019-07-29 15:52:13 -0700

    Validations for story 109

---
+++ validations.fnCheckProdFamForCNMeterInstances.sql
@@ -0,0 +1,72 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Multi-Statement Function (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the function.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Tripathi, Shubhang
+-- Create date: 29 July 2019
+-- Description:	Returns a list of SKUs in China Region which do not have Product Family with the  string 'Azure China OE'
+--				More description could be found at https://dev.azure.com/AzureReleaseOperation/Release%20Implementation/_workitems/edit/109
+-- =============================================
+CREATE FUNCTION [validations].[fnCheckProdFamForCNMeterInstances]
+(
+	
+)
+RETURNS @rtnTable TABLE (
+	[Event ID] INT NULL,
+	[Work Item Type] nvarchar(max) null,
+	[Work Item ID] INT NULL,
+	[Validation Name] nvarchar(max) NOT NULL,	
+	[Flagged Column Name] nvarchar(max) NULL,
+	[Flagged Column Value] nvarchar(max) null,
+	[Remarks] nvarchar(max) NULL,
+	[SKU State] nvarchar(max) NULL,	
+	[SAP Rate Start Date] datetime NULL,
+	[Cayman Release] nvarchar(max) NULL,	
+	[Meter Status] nvarchar(max) NULL	
+	
+)
+AS
+BEGIN
+	-- Fill the table variable with the rows for your result set
+		INSERT into @rtnTable
+		SELECT
+		[Event ID] = e.[ID],
+		[Work Item Type] = 'Consumption SKU',
+	[Work Item ID] = s.ConsumptionSkuID,
+
+	[Validation Name] = 'Product Family name should line up with the Instance name',	
+	[Flagged Column Name] = 'Product Family',
+	[Flagged Column Value] = s.[Product Family],							 
+	[Remarks] = 'China Consumption SKUs should have PFAM as "Azure China OE"',
+	[SKU State] = s.[State],	
+	[SAP Rate Start Date] = e.[SAP Rate Start Date],
+	[Cayman Release] = e.[Cayman Release],
+	[Meter Status]  = m.[Meter Status]
+		
+	
+FROM 
+		[dbASOMS_Production].[Prod].[vwASOMSEvent] e (NOLOCK) 
+		JOIN [dbASOMS_Production].[Prod].[vwASOMSMeterHist] m (NOLOCK)				ON m.[Parent id] = e.[ID] 
+		JOIN [dbASOMS_Production].[Prod].[vwASOMSConsumptionSKUHist] s (NOLOCK)		ON s.[Parent ID] = m.[MeterID]
+where 
+		
+		(s.[Azure Instance] = 'China' AND s.[Product Family] <> 'Azure China OE')
+		
+
+and     e.[State] in ('Submitted', 'Reviewed', 'Approved', 'In Progress', 'On Hold') -- for things in flight
+
+RETURN 
+END
+GO
